031fee1b25c59a4d8f00dd8f2e4e0d2b65369aca
max <mayhevv@gmail.com>
2025-12-01 13:46:14 +0200

    day one part two

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -8,6 +8,7 @@ val input = Path("src/Day01/day_01_input.txt")
 fun main() {
     val lines = input.readLines()
     partOne(lines)
+    partTwo(lines)
 }
 
 private fun partOne(lines: List<String>) {
@@ -28,6 +29,36 @@ private fun partOne(lines: List<String>) {
                 dial -= 100
             }
             if (dial == 0) count++
+        }
+    }
+    println(count)
+}
+
+fun partTwo(lines: List<String>) {
+    var count = 0
+    var dial = 50
+    for (line in lines) {
+        val direction = line.first()
+        var number = line.substringAfter(direction).toInt()
+        if (direction == 'L') {
+            if (dial == 0) count--
+            count += number / 100
+            number %= 100
+            dial -= number
+            if (dial < 0) {
+                dial += 100
+                count++
+            } else if (dial == 0) {
+                count++
+            }
+        } else {
+            count += number / 100
+            number %= 100
+            dial += number
+            if (dial > 99) {
+                dial -= 100
+                count++
+            }
         }
     }
     println(count)
