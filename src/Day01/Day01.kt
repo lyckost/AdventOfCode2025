631646ef9e68cf29af0d657e73e6613ea32cc308
max <mayhevv@gmail.com>
2025-12-01 09:58:56 +0200

    day one part one

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,1 +1,34 @@
 package Day01
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+val input = Path("src/Day01/day_01_input.txt")
+
+fun main() {
+    val lines = input.readLines()
+    partOne(lines)
+}
+
+private fun partOne(lines: List<String>) {
+    var count = 0
+    var dial = 50
+    for (line in lines) {
+        val direction = line.first()
+        val number = line.substringAfter(direction).toInt() % 100
+        if (direction == 'L') {
+            dial -= number
+            if (dial < 0) {
+                dial += 100
+            }
+            if (dial == 0) count++
+        } else {
+            dial += number
+            if (dial > 99) {
+                dial -= 100
+            }
+            if (dial == 0) count++
+        }
+    }
+    println(count)
+}
