001d23f32bdcd81a3359cc7e5fd018b3ff0125e3
Max Kuznetsov <max.kuznetsov@wolt.com>
2025-12-07 21:20:44 +0200

    day seven part two

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -7,6 +7,7 @@ val input = Path("src/Day07/day_07_input.txt")
 
 fun main() {
     partOne()
+    partTwo()
 }
 
 // 1539
@@ -39,3 +40,46 @@ fun partOne() {
     }
     println(count)
 }
+
+// 6479180385864
+fun partTwo() {
+    val lines = input.readLines()
+    val matrix = lines.map { string ->
+        string.toCharArray().map { it.toString() }.toMutableList()
+    }.toMutableList()
+    var i = 0
+    val beamPositions = mutableSetOf<Pair<Int, Int>>()
+    val hashMap = mutableMapOf<Position, Long>()
+    while (i < matrix.size) {
+        var j = 0
+        while (j < matrix[i].size) {
+            if (matrix[i][j] == "S") {
+                beamPositions.add(i + 1 to j)
+                val position = Position(i + 1, j)
+                hashMap[position] = hashMap.getOrDefault(position, 0L) + 1L
+                matrix[i + 1][j] = hashMap[position].toString()
+                break
+            }
+            if (matrix[i][j] == "^") {
+                val currentPosition = Position(i - 1, j)
+                val currentValue = hashMap.getOrDefault(currentPosition, 0L)
+                val leftResult = hashMap.getOrDefault(Position(i, j - 1), 0L) + currentValue
+                hashMap[Position(i, j - 1)] = leftResult
+                val rightResult = hashMap.getOrDefault(Position(i, j + 1), 0L) + currentValue
+                hashMap[Position(i, j + 1)] = rightResult
+                matrix[i][j - 1] = leftResult.toString()
+                matrix[i][j + 1] = rightResult.toString()
+            } else if ("\\d+".toRegex().matches(matrix.getOrNull(i - 1)?.getOrNull(j).orEmpty())) {
+                hashMap[Position(i, j)] = hashMap.getOrDefault(Position(i, j), 0L) +
+                        hashMap.getOrDefault(Position(i - 1, j), 0L)
+                matrix[i][j] = hashMap[Position(i, j)].toString()
+            }
+            j++
+        }
+        i++
+    }
+
+    println(hashMap.filter { it.key.x == matrix.size - 1 }.values.sum())
+}
+
+data class Position(val x: Int, val y: Int)
