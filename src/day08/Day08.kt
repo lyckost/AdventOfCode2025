96f0233b07cf61c50c10c91ff82c0691890a2520
max <mayhevv@gmail.com>
2025-12-08 23:09:37 +0200

    part one with union find

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -6,7 +6,8 @@ import kotlin.io.path.readLines
 val input = Path("src/Day08/day_08_input.txt")
 
 fun main() {
-    partOne()
+//    partOne()
+    partOneUnion()
 }
 
 //54600
@@ -50,4 +51,62 @@ fun partOne() {
         acc * i
     }
     println(result)
+}
+
+fun partOneUnion() {
+    val lines = input.readLines()
+    val src = lines.map { line -> line.split(",").map { it.toLong() } }
+    val connectedBoxes = mutableMapOf<Long, Pair<Int, Int>>()
+    for (i in 0 until src.size) {
+        val a = src[i]
+        for (j in i + 1 until src.size) {
+            val b = src[j]
+            val distance = a.zip(b)
+                .sumOf { (a, b) -> (a - b) * (a - b) }
+            connectedBoxes.putIfAbsent(distance, Pair(i, j))
+        }
+    }
+
+    val sortedBoxes = connectedBoxes.entries.sortedBy { it.key }
+        .take(10)
+        .associate { it.toPair() }
+
+    val uf = UnionFind(connectedBoxes.size)
+    for ((key, value) in sortedBoxes) {
+        uf.union(value.first, value.second)
+    }
+
+    val groups = mutableMapOf<Int, MutableList<Int>>()
+
+    for (i in 0 until connectedBoxes.size) {
+        val root = uf.find(i)
+        groups.getOrPut(root) { mutableListOf() }.add(i)
+    }
+
+    for ((root, members) in groups) {
+        println("Root $root: $members size is ${members.size}")
+    }
+
+    val result = groups.map { it.value.size }.sortedDescending().take(3).fold(1) { acc, i ->
+        acc * i
+    }
+    println("result = $result")
+}
+
+class UnionFind(n: Int) {
+    val parent = IntArray(n) { it }
+
+    fun find(x: Int): Int {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+
+    fun union(a: Int, b: Int) {
+        val rootA = find(a)
+        val rootB = find(b)
+
+        parent[rootA] = rootB
+    }
 }
